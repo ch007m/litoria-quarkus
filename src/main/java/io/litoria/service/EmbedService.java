d145a3730d63c1e709c4b3722a5bb032cff3302e
Charles Moulliard <cmoulliard@ibm.com>
2026-08-17 13:42:09 +0200

    Remove ununsued import
    Signed-off-by: Charles Moulliard <cmoulliard@ibm.com>

--- src/main/java/io/litoria/service/EmbedService.java
+++ src/main/java/io/litoria/service/EmbedService.java
@@ -2,7 +2,6 @@ package io.litoria.service;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.geom.Ellipse2D;
